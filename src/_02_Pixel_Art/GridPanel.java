113e65864bde7d8bcc3dad586c9b3f751a12ba68
Sofia Velarde <sofiavelarde2004@gmail.com>
2020-02-09 17:26:21 -0800

    Pixel_Art (Finish), ConwaysGame(IP)
    left off on step 8

--- src/_02_Pixel_Art/GridPanel.java
+++ src/_02_Pixel_Art/GridPanel.java
@@ -41,7 +41,7 @@ public class GridPanel extends JPanel {
 		// 3. Iterate through the array and initialize each element to a new pixel.
 		for (int i = 0; i < pixel.length; i++) {
 			for (int j = 0; j < pixel[i].length; j++) {
-				pixel[i][j] = new Pixel(pixelWidth,pixelHeight);
+				pixel[i][j] = new Pixel(i,j);
 
 			}
 
@@ -56,7 +56,7 @@ public class GridPanel extends JPanel {
 	public void clickPixel(int mouseX, int mouseY) {
 		// 5. Use the mouseX and mouseY variables to change the color
 		// of the pixel that was clicked. *HINT* Use the pixel's dimensions.
-
+		System.out.println(mouseX/pixelWidth);
 		pixel[mouseX/pixelWidth][mouseY/pixelHeight].color = color;
 		
 	}
@@ -68,9 +68,10 @@ public class GridPanel extends JPanel {
 		// Then, use drawRect to add a grid pattern to your display.
 		for (int i = 0; i < pixel.length; i++) {
 			for (int k = 0; k < pixel.length; k++) {
-				g.fillRect(i*pixelWidth, i*pixelHeight, pixelWidth, pixelHeight);
-				g.drawRect(i*pixelWidth, i*pixelHeight, pixelWidth, pixelHeight);
-				// FILL IN RECTANGLE
+				g.setColor(pixel[i][k].color);
+				g.fillRect(i*pixelWidth, k*pixelHeight, pixelWidth, pixelHeight);
+				g.setColor(color.BLACK);
+				g.drawRect(i*pixelWidth, k*pixelHeight, pixelWidth, pixelHeight);
 			}
 
 		}
